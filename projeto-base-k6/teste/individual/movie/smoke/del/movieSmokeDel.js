715b987edfa851c71a7eaeb918c4544f946626ba
thiagofoganholo <you@example.com>
2023-09-29 23:38:47 -0300

    style(moviesmokedel.js): remove a code that is not need anymore
    i just removed random generator, it was not needed more

--- projeto-base-k6/teste/individual/movie/smoke/del/movieSmokeDel.js
+++ projeto-base-k6/teste/individual/movie/smoke/del/movieSmokeDel.js
@@ -2,7 +2,6 @@ import { sleep } from 'k6';
 import { SharedArray } from 'k6/data';
 import {BaseChecks, BaseRest, BaseService, ENDPOINTS, testeConfig} from '../../../../../../projeto-base-k6/support/base/baseTest.js'
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 const base_uri = testeConfig.environments.hml.url;
 const baseRest = new BaseRest(base_uri);
@@ -32,9 +31,6 @@ export function setup() {
 export default function () {
 
   const {id_array} = setup();
-
-  const randomMovieID = randomItem(id_array);
-
   
   id_array.forEach((id) => {
     //console.log(id);
