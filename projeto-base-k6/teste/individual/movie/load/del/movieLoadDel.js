74c3e4dd1be9dda5783c9261f2bda878f7fd643b
thiagofoganholo <you@example.com>
2023-10-02 17:27:02 -0300

    fix(moviesmokedel.js movieloaddel.js moviestressdel.js): fix a bug on delete res
    i was having a lot of bugs with Res, now i fix and is it works kind fine.

--- projeto-base-k6/teste/individual/movie/load/del/movieLoadDel.js
+++ projeto-base-k6/teste/individual/movie/load/del/movieLoadDel.js
@@ -2,6 +2,7 @@ import { sleep } from 'k6';
 import { SharedArray } from 'k6/data';
 import {BaseChecks, BaseRest, BaseService, ENDPOINTS, testeConfig} from '../../../../../../projeto-base-k6/support/base/baseTest.js'
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 const base_uri = testeConfig.environments.hml.url;
 const baseRest = new BaseRest(base_uri);
@@ -13,13 +14,11 @@ const data_movies = new SharedArray('movie', function () {
     const jsonData = JSON.parse(open('../../../../../../../k6/projeto-base-k6/data/dynamic/movie.json'));
     return jsonData; // must be an array
 });
-
 export function handleSummary(data) {
   return {
     "summary.html": htmlReport(data),
   };
 }
-
 export function setup() { 
   const res = baseRest.get(ENDPOINTS.MOVIES);
   const jsonData = JSON.parse(res.body);
@@ -27,20 +26,22 @@ export function setup() {
   
   return {id_array};
 }
-
 export default function () {
 
   const {id_array} = setup();
+
+  const randomMovieID = randomItem(id_array);
+
+  //console.log(randomMovieID)
   
-  id_array.forEach((id) => {
-    //console.log(id);
-    const resDelMovie = baseRest.delete(ENDPOINTS.MOVIES + `/${id}`);
+  const resDelMovie = baseRest.delete(ENDPOINTS.MOVIES + `/${randomMovieID}`);
+  
+  //console.log(resDelMovie.status);
+
+  baseChecks.checkStatusCode(resDelMovie, 200);
+  baseChecks.checkResponseTime(resDelMovie, 400);
     
-    baseChecks.checkStatusCode(resDelMovie, 200);
-    baseChecks.checkResponseTime(resDelMovie, 400);
-  });
+    
   
     sleep(1);
 }
-
-
